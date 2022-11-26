60e23f67b8cc81a79a69c706dd898c8d90cfda40
Nils Alex <nils@famalex.de>
2022-11-26 16:05:28 +0100

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+YEAR=2021
+
 ROOT_DIR="$(dirname "$(realpath "$0")")"
 
 if [[ "${ROOT_DIR}" != "${PWD}" ]];
@@ -10,21 +12,34 @@ then
   exit 1
 fi
 
+if [[ ! -f .session ]]
+then
+  echo "File .session does not exist. Please provide it according to README.md."
+  exit 1
+fi
 source .session
 HEADER="Cookie: session=${SESSION}"
 
-DAY=1
-YEAR=2021
-INPUT_DIR="day/$(printf "%02d" $DAY)"
-INPUT_FILE="${INPUT_DIR}/input"
-INPUT_URL="https://adventofcode.com/${YEAR}/${DAY}/input"
+DAY="${1}"
+PACKAGE_DIR="day$(printf "%02d" "${DAY}")"
+INPUT_FILE="${PACKAGE_DIR}/input.txt"
+INPUT_URL="https://adventofcode.com/${YEAR}/day/${DAY}/input"
 
-mkdir -p "$INPUT_DIR"
-echo "Created directory ${INPUT_DIR}"
+if [[ -e ${PACKAGE_DIR} ]]
+then
+  echo "Package directory ${PACKAGE_DIR} already exists. Skipping package initialization."
+else
+  cargo new \
+        --bin \
+        --edition 2021 \
+        --quiet \
+        "${PACKAGE_DIR}"
+  echo "Initialized cargo package at ${PACKAGE_DIR}"
+fi
 
 curl --silent \
      -XGET \
      -H"${HEADER}" \
      --output "${INPUT_FILE}" \
      "${INPUT_URL}"
-echo "Dowloaded input from ${INPUT_URL} to ${INPUT_FILE}"
+echo "Downloaded input from ${INPUT_URL} to ${INPUT_FILE}. Character count: $(wc -c <"${INPUT_FILE}") "
