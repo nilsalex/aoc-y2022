b949f574fa9f3ef433860da337dd5875aabd84a1
Nils Alex <nils@famalex.de>
2022-12-01 18:09:49 +0100

    More functional approach to day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,42 +1,19 @@
 pub const INPUT: &str = include_str!("input.txt");
 
 fn part1(input: &str) -> usize {
-    let mut max_cals = 0;
-    let mut cur_cals = 0;
-
     input
-        .lines()
-        .for_each(|l| {
-            if l.is_empty() {
-                if cur_cals > max_cals {
-                    max_cals = cur_cals
-                }
-                cur_cals = 0
-            } else {
-                cur_cals += l.parse::<usize>().unwrap();
-            }
-        }
-        );
-    max_cals
+        .split("\n\n")
+        .map(|group| group.lines().map(|l| l.parse::<usize>().unwrap()).sum())
+        .max().unwrap()
 }
 
 fn part2(input: &str) -> usize {
-    let mut sums: Vec<usize> = Vec::new();
-    sums.push(0);
-
-    input
-        .lines()
-        .for_each(|l| {
-            if l.is_empty() {
-                sums.push(0)
-            } else {
-                let cals: usize = l.parse().unwrap();
-                *sums.last_mut().unwrap() += cals;
-            }
-        }
-        );
-    sums.sort_by(|a, b| b.cmp(a));
-    sums[0] + sums[1] + sums[2]
+    let mut cals = input
+        .split("\n\n")
+        .map(|group| group.lines().map(|l| l.parse::<usize>().unwrap()).sum())
+        .collect::<Vec<usize>>();
+    cals.select_nth_unstable_by(3, |a,b| b.cmp(a));
+    cals.iter().take(3).sum()
 }
 
 fn main() {
