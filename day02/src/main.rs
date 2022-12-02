0f93d63b62b2186e8007a1e1967901c6dff69a7d
Nils Alex <nils@famalex.de>
2022-12-02 10:05:24 +0100

    Use bytes as input for day 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,26 +1,25 @@
-pub const INPUT: &str = include_str!("input.txt");
+pub const INPUT: &[u8] = include_bytes!("input.txt");
 
+const NEWLINE: u8 = 10;
 const CAPITAL_A: u8 = 65;
 const CAPITAL_Z: u8 = 88;
 
-fn part1(input: &str) -> u32 {
+fn part1(input: &[u8]) -> u32 {
     input
-        .lines()
-        .map(|l| {
-            let bytes = l.as_bytes();
-            let a = bytes[0] - CAPITAL_A;
-            let b = bytes[2] - CAPITAL_Z;
+        .split_inclusive(|byte| *byte == NEWLINE)
+        .map(|line| {
+            let a = line[0] - CAPITAL_A;
+            let b = line[2] - CAPITAL_Z;
             (1 + b + ((4 + b - a) % 3) * 3) as u32 // add 4 instead of 1 because 4 + b - a is always > 0
         }).sum()
 }
 
-fn part2(input: &str) -> u32 {
+fn part2(input: &[u8]) -> u32 {
     input
-        .lines()
-        .map(|l| {
-            let bytes = l.as_bytes();
-            let a = bytes[0] - CAPITAL_A;
-            let b = bytes[2] - CAPITAL_Z;
+        .split_inclusive(|byte| *byte == NEWLINE)
+        .map(|line| {
+            let a = line[0] - CAPITAL_A;
+            let b = line[2] - CAPITAL_Z;
             (1 + 3 * b + (a + b + 2) % 3) as u32
         }).sum()
 }
