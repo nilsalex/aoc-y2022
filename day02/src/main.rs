6af084a55de59873695ddbf4915932a9c2f338d8
Nils Alex <nils@famalex.de>
2022-12-04 14:46:49 +0100

    trim bytes and use split instead of split_inclusive in day 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,29 +1,29 @@
 #![feature(test)]
+#![feature(byte_slice_trim_ascii)]
 
 pub const INPUT: &[u8] = include_bytes!("input.txt");
 
-const NEWLINE: u8 = 10;
-const CAPITAL_A: u8 = 65;
-const CAPITAL_Z: u8 = 88;
-
 fn part1(input: &[u8]) -> u32 {
     input
-        .split_inclusive(|byte| *byte == NEWLINE)
-        .map(|line| {
-            let a = line[0] - CAPITAL_A;
-            let b = line[2] - CAPITAL_Z;
+        .trim_ascii_end()
+        .split(|byte| *byte == b'\n')
+        .map(|bytes| {
+            let a = bytes[0] - b'A';
+            let b = bytes[2] - b'Z';
             (1 + b + ((4 + b - a) % 3) * 3) as u32 // add 4 instead of 1 because 4 + b - a is always > 0
-        }).sum()
+        })
+        .sum()
 }
 
 fn part2(input: &[u8]) -> u32 {
     input
-        .split_inclusive(|byte| *byte == NEWLINE)
-        .map(|line| {
-            let a = line[0] - CAPITAL_A;
-            let b = line[2] - CAPITAL_Z;
+        .split_inclusive(|byte| *byte == b'\n')
+        .map(|bytes| {
+            let a = bytes[0] - b'A';
+            let b = bytes[2] - b'Z';
             (1 + 3 * b + (a + b + 2) % 3) as u32
-        }).sum()
+        })
+        .sum()
 }
 
 fn main() {
