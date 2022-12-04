6805c878d446e7117bf8a2b8f5a8a07d67acda81
Nils Alex <nils@famalex.de>
2022-12-04 14:19:19 +0100

    faster solution for day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,20 +1,47 @@
 #![feature(test)]
+#![feature(byte_slice_trim_ascii)]
 extern crate test;
 
-pub const INPUT: &str = include_str!("input.txt");
+pub const INPUT: &[u8] = include_bytes!("input.txt");
 
-fn part1(input: &str) -> usize {
-    input
-        .split("\n\n")
-        .map(|group| group.lines().map(|l| l.parse::<usize>().unwrap()).sum())
-        .max().unwrap()
+const POWERS_OF_TEN: [u32; 10] = [
+    1, 10, 100, 1000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000
+];
+
+fn u32_from_bytes(bytes: &[u8]) -> u32 {
+    bytes.iter().rev().enumerate().fold(0, |acc, (ix, x)| acc + (x - b'0') as u32 * POWERS_OF_TEN[ix])
 }
 
-fn part2(input: &str) -> usize {
-    let mut cals = input
-        .split("\n\n")
-        .map(|group| group.lines().map(|l| l.parse::<usize>().unwrap()).sum())
-        .collect::<Vec<usize>>();
+fn part1(input: &[u8]) -> u32 {
+    let mut max_cals_sum: u32 = 0;
+    let mut cur_cals_sum: u32 = 0;
+
+    for bytes in input.trim_ascii_end().split(|byte| *byte == b'\n') {
+        if bytes.is_empty() {
+            max_cals_sum = std::cmp::max(max_cals_sum, cur_cals_sum);
+            cur_cals_sum = 0
+        } else {
+            cur_cals_sum += u32_from_bytes(bytes)
+        }
+    }
+
+    max_cals_sum
+}
+
+fn part2(input: &[u8]) -> u32 {
+    let mut cals = Vec::new();
+
+    let mut cur_cals_sum: u32 = 0;
+
+    for bytes in input.trim_ascii_end().split(|byte| *byte == b'\n') {
+        if bytes.is_empty() {
+            cals.push(cur_cals_sum);
+            cur_cals_sum = 0
+        } else {
+            cur_cals_sum += u32_from_bytes(bytes)
+        }
+    }
+
     cals.select_nth_unstable_by(3, |a, b| b.cmp(a));
     cals.iter().take(3).sum()
 }
