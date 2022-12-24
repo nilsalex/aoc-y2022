d2b6571b85568312604056c585ba7c12d057f09a
Nils Alex <nils@famalex.de>
2022-12-25 00:37:03 +0100

    sub-ms solution for day 24 using pre-allocated bit arrays

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -2,60 +2,42 @@
 #![feature(test)]
 extern crate test;
 
-use std::cmp::{Ordering, Reverse};
-use std::collections::{BinaryHeap, HashSet};
-
 const INPUT: &[u8] = include_bytes!("input.txt");
-// const INPUT: &[u8] = include_bytes!("input_test.txt");
+const MAX_NUM_ROWS: usize = 32;
 
-#[derive(Clone)]
+const fn shift_bits_right_with_wrapping(bits: u128, num_bits: usize) -> u128 {
+    (bits >> 1) | (bits & 1_u128) << (num_bits - 1)
+}
+
+const fn shift_bits_left_with_wrapping(bits: u128, num_bits: usize) -> u128 {
+    let mask = 1_u128 << (num_bits - 1);
+    let left_bits = bits & mask;
+    let remaining_bits = bits & !mask;
+    (remaining_bits << 1) | left_bits >> (num_bits - 1)
+}
+
 struct Grid {
-    arrows_up: Vec<Vec<isize>>,
-    arrows_down: Vec<Vec<isize>>,
-    arrows_left: Vec<Vec<isize>>,
-    arrows_right: Vec<Vec<isize>>,
-    num_rows: isize,
-    num_cols: isize,
-}
-
-#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
-struct Position {
-    row: isize,
-    col: isize,
-}
-
-impl Position {
-    fn dist(&self, other: &Self) -> usize {
-        self.row.abs_diff(other.row) + self.col.abs_diff(other.col)
-    }
-}
-
-impl Ord for Position {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.row.cmp(&other.row).then(self.col.cmp(&other.col))
-    }
-}
-
-impl PartialOrd for Position {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
+    arrows_up: [u128; MAX_NUM_ROWS],
+    arrows_down: [u128; MAX_NUM_ROWS],
+    arrows_left: [u128; MAX_NUM_ROWS],
+    arrows_right: [u128; MAX_NUM_ROWS],
+    positions: [u128; MAX_NUM_ROWS],
+    start_bit: bool,
+    end_bit: bool,
+    num_rows: usize,
+    num_cols: usize,
 }
 
 impl Grid {
     fn parse(input: &[u8]) -> Self {
-        let num_cols = input.iter().position(|byte| *byte == b'\n').unwrap() - 2;
-        let num_rows = input
-            .trim_ascii_end()
-            .iter()
-            .filter(|byte| **byte == b'\n')
-            .count()
-            - 1;
+        let mut arrows_up = [0_u128; MAX_NUM_ROWS];
+        let mut arrows_down = [0_u128; MAX_NUM_ROWS];
+        let mut arrows_left = [0_u128; MAX_NUM_ROWS];
+        let mut arrows_right = [0_u128; MAX_NUM_ROWS];
+        let positions = [0_u128; MAX_NUM_ROWS];
 
-        let mut arrows_up: Vec<Vec<isize>> = vec![Vec::new(); num_cols];
-        let mut arrows_down: Vec<Vec<isize>> = vec![Vec::new(); num_cols];
-        let mut arrows_left: Vec<Vec<isize>> = vec![Vec::new(); num_rows];
-        let mut arrows_right: Vec<Vec<isize>> = vec![Vec::new(); num_rows];
+        let num_cols = input.iter().position(|byte| *byte == b'\n').unwrap() - 2;
+        let num_rows = input.trim_ascii_end().split(|byte| *byte == b'\n').count() - 2;
 
         input
             .trim_ascii_end()
@@ -64,203 +46,120 @@ impl Grid {
             .take(num_rows)
             .enumerate()
             .for_each(|(row, line)| {
-                line.iter()
-                    .skip(1)
-                    .take(num_cols)
-                    .enumerate()
-                    .for_each(|(col, byte)| match byte {
-                        b'^' => arrows_up[col].push(row as isize),
-                        b'v' => arrows_down[col].push(row as isize),
-                        b'<' => arrows_left[row].push(col as isize),
-                        b'>' => arrows_right[row].push(col as isize),
-                        b'.' => {}
-                        _ => panic!(),
-                    })
+                {
+                    line.iter().skip(1).take(num_cols).enumerate().for_each(
+                        |(col, byte)| match byte {
+                            b'^' => arrows_up[row] |= 1_u128 << col,
+                            b'v' => arrows_down[row] |= 1_u128 << col,
+                            b'<' => arrows_left[row] |= 1_u128 << col,
+                            b'>' => arrows_right[row] |= 1_u128 << col,
+                            b'.' => {}
+                            _ => panic!(),
+                        },
+                    )
+                }
             });
-
-        let num_rows = num_rows as isize;
-        let num_cols = num_cols as isize;
 
         Self {
             arrows_up,
             arrows_down,
             arrows_left,
             arrows_right,
+            positions,
+            start_bit: false,
+            end_bit: false,
             num_rows,
             num_cols,
         }
     }
 
-    fn in_bounds(&self, pos: &Position) -> bool {
-        pos.col >= 0 && pos.row >= 0 && pos.col < self.num_cols && pos.row < self.num_rows
-            || pos.row == -1 && pos.col == 0
-            || pos.row == self.num_rows && pos.col == self.num_cols - 1
-    }
+    fn step(&mut self) {
+        self.arrows_up[0..self.num_rows].rotate_left(1);
+        self.arrows_down[0..self.num_rows].rotate_right(1);
 
-    fn is_allowed(&self, pos: &Position, step: usize) -> bool {
-        if pos.row == -1 && pos.col == 0 {
-            return true;
+        for col in self.arrows_left.iter_mut() {
+            *col = shift_bits_right_with_wrapping(*col, self.num_cols);
+        }
+        for col in self.arrows_right.iter_mut() {
+            *col = shift_bits_left_with_wrapping(*col, self.num_cols);
         }
 
-        if pos.row == self.num_rows && pos.col == self.num_cols - 1 {
-            return true;
+        let previous_start = self.start_bit;
+        let previous_end = self.end_bit;
+
+        if self.positions[0] & 1_u128 != 0_u128 {
+            self.start_bit = true
         }
 
-        for arrow in &self.arrows_up[pos.col as usize] {
-            if (arrow - step as isize).rem_euclid(self.num_rows) == pos.row {
-                return false;
-            }
+        if self.positions[self.num_rows - 1] & (1_u128 << (self.num_cols - 1)) != 0_u128 {
+            self.end_bit = true
         }
 
-        for arrow in &self.arrows_down[pos.col as usize] {
-            if (arrow + step as isize).rem_euclid(self.num_rows) == pos.row {
-                return false;
+        let mut previous_col = 0_u128;
+        for row in 0..self.num_rows {
+            let current_col = self.positions[row];
+            self.positions[row] |= previous_col | (current_col >> 1) | (current_col << 1);
+            previous_col = current_col;
+            if row + 1 < self.num_rows {
+                self.positions[row] |= self.positions[row + 1]
             }
+            self.positions[row] &= !(1_u128 << self.num_cols);
+            self.positions[row] &= !(self.arrows_up[row]
+                | self.arrows_down[row]
+                | self.arrows_left[row]
+                | self.arrows_right[row])
         }
 
-        for arrow in &self.arrows_left[pos.row as usize] {
-            if (arrow - step as isize).rem_euclid(self.num_cols) == pos.col {
-                return false;
-            }
+        if previous_start {
+            self.positions[0] |= 1_u128
         }
 
-        for arrow in &self.arrows_right[pos.row as usize] {
-            if (arrow + step as isize).rem_euclid(self.num_cols) == pos.col {
-                return false;
-            }
-        }
-
-        true
-    }
-}
-
-#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
-struct State {
-    depth: usize,
-    position: Position,
-}
-
-impl Ord for State {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.depth
-            .cmp(&other.depth)
-            .then(self.position.cmp(&other.position))
-    }
-}
-
-impl PartialOrd for State {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-fn bfs(grid: &Grid, initial_state: &State, target_pos: &Position) -> State {
-    let mut queue: BinaryHeap<Reverse<(usize, State)>> = BinaryHeap::new();
-    let mut visited: HashSet<State> = HashSet::new();
-
-    queue.push(Reverse((
-        initial_state.position.dist(target_pos),
-        *initial_state,
-    )));
-    visited.insert(*initial_state);
-
-    loop {
-        if let Some(Reverse((_, state))) = queue.pop() {
-            if &state.position == target_pos {
-                break state;
-            }
-
-            let next_states: Vec<State> = [
-                State {
-                    position: Position {
-                        row: state.position.row - 1,
-                        col: state.position.col,
-                    },
-                    depth: state.depth + 1,
-                },
-                State {
-                    position: Position {
-                        row: state.position.row + 1,
-                        col: state.position.col,
-                    },
-                    depth: state.depth + 1,
-                },
-                State {
-                    position: Position {
-                        row: state.position.row,
-                        col: state.position.col - 1,
-                    },
-                    depth: state.depth + 1,
-                },
-                State {
-                    position: Position {
-                        row: state.position.row,
-                        col: state.position.col + 1,
-                    },
-                    depth: state.depth + 1,
-                },
-                State {
-                    position: state.position,
-                    depth: state.depth + 1,
-                },
-            ]
-            .into_iter()
-            .filter(|state| {
-                grid.in_bounds(&state.position) && grid.is_allowed(&state.position, state.depth)
-            })
-            .collect();
-
-            for next_state in next_states {
-                if !visited.contains(&next_state) {
-                    let heuristic = next_state.depth + next_state.position.dist(target_pos);
-                    visited.insert(next_state);
-                    queue.push(Reverse((heuristic, next_state)));
-                }
-            }
-        } else {
-            panic!()
+        if previous_end {
+            self.positions[self.num_rows - 1] |= 1_u128 << (self.num_cols - 1)
         }
     }
 }
 
 fn part1(input: &[u8]) -> usize {
-    let grid = Grid::parse(input);
+    let mut grid = Grid::parse(input);
+    grid.start_bit = true;
 
-    let begin_pos = Position { row: -1, col: 0 };
-    let end_pos = Position {
-        row: grid.num_rows,
-        col: grid.num_cols - 1,
-    };
+    let mut time = 0;
+    while !grid.end_bit {
+        time += 1;
+        grid.step();
+    }
 
-    let initial_state = State {
-        depth: 0,
-        position: begin_pos,
-    };
-
-    let final_state = bfs(&grid, &initial_state, &end_pos);
-
-    final_state.depth
+    time
 }
 
 fn part2(input: &[u8]) -> usize {
-    let grid = Grid::parse(input);
+    let mut grid = Grid::parse(input);
+    let mut time = 0;
 
-    let begin_pos = Position { row: -1, col: 0 };
-    let end_pos = Position {
-        row: grid.num_rows,
-        col: grid.num_cols - 1,
-    };
+    grid.start_bit = true;
+    while !grid.end_bit {
+        time += 1;
+        grid.step();
+    }
 
-    let initial_state = State {
-        depth: 0,
-        position: begin_pos,
-    };
+    grid.positions.fill(0);
+    grid.start_bit = false;
+    grid.end_bit = true;
+    while !grid.start_bit {
+        time += 1;
+        grid.step();
+    }
 
-    let there = bfs(&grid, &initial_state, &end_pos);
-    let and_back_again = bfs(&grid, &there, &begin_pos);
-    let and_there_again = bfs(&grid, &and_back_again, &end_pos);
+    grid.positions.fill(0);
+    grid.start_bit = true;
+    grid.end_bit = false;
+    while !grid.end_bit {
+        time += 1;
+        grid.step();
+    }
 
-    and_there_again.depth
+    time
 }
 
 fn main() {
@@ -274,6 +173,18 @@ mod tests {
 
     use super::*;
     use test::Bencher;
+
+    #[test]
+    fn test_shift_right() {
+        let bits: u128 = 0b0000000010000101;
+        assert_eq!(shift_bits_right_with_wrapping(bits, 8), 0b0000000011000010);
+    }
+
+    #[test]
+    fn test_shift_left() {
+        let bits: u128 = 0b0000000010000101;
+        assert_eq!(shift_bits_left_with_wrapping(bits, 8), 0b0000000000001011);
+    }
 
     #[test]
     fn test_part1() {
